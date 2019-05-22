e4e3e1fc9b3577942b9e98005bfec5740ce99565
prestontewell <preston@tewellphoto.com>
2019-05-22 11:32:27 -0500

    refactor code to loop forever until user inputs Bob

--- ruby.rb
+++ ruby.rb
@@ -21,4 +21,33 @@ while (i < numbers.length)
 	sum = sum + number
 	i += 1
 end
-p sum
+# p sum
+
+# The following code will run 100 times, and each time it will ask the user for their name. The code will break early if the user’s name is Bob.
+
+# 100.times do
+  # puts "What is your name?"  
+  # name = gets.chomp
+  # if name == "Bob"
+    # break
+  # end
+# end
+
+# puts "Hi, Bob!"
+
+# Rewrite the code using a while loop so the program will run *forever* unless the user enters a name of Bob.
+
+p "What is your name?"
+name = gets.chomp
+while (name != "Bob")
+	p "You're not Bob"
+	p "What is your name?"
+	name = gets.chomp
+	if name == "Bob"
+		break
+	end
+end
+
+p "Hi, Bob"
+
+
